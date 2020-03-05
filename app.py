c89210eadc88a2b39b4ace0f349657958a58d513
MohammadAmin Hakim <mohammadaminonline@gmail.com>
2020-03-05 19:08:04 +0330

    add main function

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ app = Flask(__name__)
 def main_page():
         
         return 'Hello'
+
+@app.route('/v1/process')
+def getsms():
+        pass
+
+def send_sms():
+        pass
+
+def check_serial():
+        pass
+
+if __name__ == "__main__":
+        app.run("0.0.0.0, 5000")
+        
